a5c1f82928f28fbc47fb4cca12caf0b6f2d86452
Gurveen1724 <109553192+Gurveen1724@users.noreply.github.com>
2022-08-10 14:32:30 -0400

    Update resgrp1.tf

--- resgrp1.tf
+++ resgrp1.tf
@@ -1,7 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
 provider "azurerm" {
   features {}
-  }
+}
 resource "azurerm_resource_group" "rgrp1" {
   name= "myresgrup1"
   location= "eastus2"
-  }
+}
